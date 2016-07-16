afda5c37822c089df932b883aa94f51725b3bbb1
Rushaine McBean <rushaine.mcbean@gmail.com>
2016-07-16 01:15:18 -0400

    Fix linter issues

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
-import CourseList from './courseList';
+import CourseList from './CourseList';
 
 class CoursesPage extends React.Component {
 
@@ -15,7 +15,7 @@ class CoursesPage extends React.Component {
   }
 
   render() {
-    const { courses } = this.props
+    const { courses } = this.props;
     return (
       <div>
         <h1>Courses</h1>
