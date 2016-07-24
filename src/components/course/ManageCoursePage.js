bc163500823f2bbb4d8cf678a0445a96ccd77b8e
Rushaine McBean <rushaine.mcbean@gmail.com>
2016-07-24 13:46:04 -0400

    Populate course form to update a course

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -53,8 +53,21 @@ ManageCoursePage.contextTypes = {
   router: PropTypes.object
 };
 
+
+function getCourseById(courses, id) {
+  const course = courses.filter(course => course.id == id);
+  if (course.length) return course[0];
+  return null;
+}
+
 function mapStateToProps(state, ownProps) {
+  const courseId = ownProps.params.id;
+
   let course = { id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
+
+  if (courseId && state.courses.length > 0) {
+    course = getCourseById(state.courses, courseId);
+  }
 
   const authorsFormattedForDropdown = state.authors.map(author => {
       return {
