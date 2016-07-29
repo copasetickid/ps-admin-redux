be5e605e3d0df5f5795506fc8551dd5dc4d80241
Rushaine McBean <rushaine.mcbean@gmail.com>
2016-07-29 14:25:40 -0400

    Hide Preloader based on Status

--- src/reducers/ajaxStatusReducer.js
+++ src/reducers/ajaxStatusReducer.js
@@ -5,7 +5,7 @@ function actionTypeEndsInSuccess(type) {
   return type.substring(type.length - 8) == '_SUCCESS';
 }
 
-export default function (state = initialState.numAjaxCallsInProgress, action) {
+export default function ajaxStatusReducer(state = initialState.ajaxCallsInProgress, action) {
   if (action.type == types.BEGIN_AJAX_CALL) {
     return state + 1;
   } else if (actionTypeEndsInSuccess(action.type)) {
