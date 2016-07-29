474181074851dabc45abeffc172802b3d6857bbb
Rushaine McBean <rushaine.mcbean@gmail.com>
2016-07-29 14:47:15 -0400

    Handles and diplays errors

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -36,8 +36,12 @@ class ManageCoursePage extends React.Component {
   saveCourse(event) {
     event.preventDefault();
     this.setState({saving: true});
-    this.props.actions.saveCourse(this.state.course).then(() => this.redirect());
-
+    this.props.actions.saveCourse(this.state.course)
+    .then(() => this.redirect())
+    .catch(error => {
+      toastr.error(error);
+      this.setState({saving: false});
+    });
   }
 
   redirect() {
