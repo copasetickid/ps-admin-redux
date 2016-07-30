9c14568979119238f9ffb428b49841ca860eb911
Rushaine McBean <rushaine.mcbean@gmail.com>
2016-07-30 14:01:23 -0400

    Fixes typos for prod build and adds in scripts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-/*eslint-disable import-default*/
+/*eslint-disable import/default*/
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,15 +2,15 @@ import webpack from 'webpack';
 import path from 'path';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
-const GLOBALS {
-  'process.env.NODE_ENV': JSON.stringify('production');
+const GLOBALS = {
+  'process.env.NODE_ENV': JSON.stringify('production')
 };
 
 export default {
   debug: true,
   devtool: 'source-map',
   noInfo: false,
-  entry: './src/index'
+  entry: './src/index',
   target: 'web',
   output: {
     path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
@@ -30,7 +30,7 @@ export default {
   module: {
     loaders: [
       {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
-      {test: /(\.css)$/, loaders: ExtractTextPlugin("css?sourceMap")},
+      {test: /(\.css)$/, loader: ExtractTextPlugin.extract('css?sourceMap')},
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
       {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
       {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
